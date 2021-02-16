5b4986af5bf080b6bc0785817f1813d0fda6aa3d
nagqatt <nagqatt@yahoo.co.uk>
2021-02-16 01:14:07 +0000

    Updated now again

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -6,11 +6,12 @@ public class Test1 {
     @Test
     public void test1(){
         System.out.println("Running test 1");
-        Assert.assertFalse(true);
+        Assert.fail();
     }
     public void test2(){
         System.out.println("Running test 2");
         System.out.println("Added new code");
+
     }
     public void test3(){
         System.out.println("Running test 3");
