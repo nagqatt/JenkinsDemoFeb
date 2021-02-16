3d21123c20b5def88d0232171af197a45596a2a1
nagqatt <nagqatt@yahoo.co.uk>
2021-02-16 00:31:56 +0000

    Committing to Repo

---
+++ src/test/java/Test1.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+public class Test1 {
+
+    @Test
+    public void test1(){
+        System.out.println("Running test 1");
+    }
+    public void test2(){
+        System.out.println("Running test 2");
+    }
+    public void test3(){
+        System.out.println("Running test 3");
+    }
+    public void test4(){
+        System.out.println("Running test 4");
+    }
+
+}
